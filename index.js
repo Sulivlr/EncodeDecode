cebcf3d0b4ab851c1425b00e4400b1a596a47b99
Sultan Mukhtarov <suli419956@gmail.com>
2024-07-30 23:09:43 +0600

    Created an echo req with params.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require('express');
+
+
 const app = express();
 const port = 8000;
+
+app.get('/:echo', (req, res) => {
+    const echo = req.params.echo
+    res.send(echo);
+})
+
+app.listen(port, () => {
+    console.log('Listening on port ' + port);
+})
+
+
+
