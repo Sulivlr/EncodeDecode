3741ad83ea408067d9d008e02e341841b6afe4ed
Sultan Mukhtarov <suli419956@gmail.com>
2024-07-30 23:33:43 +0600

    Created vigenere decode and encoded password with params.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Vigenere = require("caesar-salad/vigenere");
 
 
 const app = express();
@@ -7,11 +8,25 @@ const port = 8000;
 app.get('/:echo', (req, res) => {
     const echo = req.params.echo
     res.send(echo);
-})
+});
+
+app.get('/encode/:text', (req, res) => {
+    const text = req.params.text;
+    const encodedText = Vigenere.Cipher('password').crypt(text);
+    res.send(`<h1>Encoded Text: ${encodedText}</h1>`);
+});
+
+app.get('/decode/:text', (req, res) => {
+    const text = req.params.text;
+    const decodedText = Vigenere.Decipher('password').crypt(text);
+    res.send(`<h1>Decoded Text: ${decodedText}</h1>`);
+});
+
+
 
 app.listen(port, () => {
-    console.log('Listening on port ' + port);
-})
+    console.log(`Server is running on port: http://localhost:${port}/`);
+});
 
 
 
